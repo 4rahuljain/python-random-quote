0a9e1701029503d69739ecd48fbd6f6f739b1ee8
4rahuljain <4rahuljain@users.noreply.github.com>
2020-05-17 17:47:41 +0530

    updated

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,15 @@
 def main():
   # print("Keep it logically awesome.")
-
+  import random 
   f = open("/Users/rahuljain/Documents/GitHub/python-random-quote/quotes.txt")
   quotes = f.readlines()
   f.close()
-
-  print(quotes)
+  
+  x=len(quotes)
+  z=x-1
+  y=random.randint(0,z)
+  quot= quotes[y]
+  print(quot)
 
 if __name__== "__main__":
   main()
